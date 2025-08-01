71d8cf6cff2508c264557e278f1fa281191aceae
labforweb <aleksandernikollibusiness@gmail.com>
2025-08-01 22:27:55 +0200

    Final configs for successful deployment

--- server.ts
+++ server.ts
@@ -35,7 +35,7 @@ export function app(): express.Express {
 }
 
 function run(): void {
-  const port = process.env['PORT'] || 4000;
+  const port = process.env['PORT'] || 8080;
   const server = app();
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
