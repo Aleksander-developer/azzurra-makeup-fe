b08e703d177e182506588f6cc16fc2ce7ed9eac2
labforweb <aleksandernikollibusiness@gmail.com>
2025-08-04 12:11:11 +0200

    feat: Update production environment with AWS backend URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://azzurra-backend-1094999227512.europe-west1.run.app' // <-- AGGIORNATO CON L'URL DEL TUO BACKEND IN PRODUZIONE
+  apiUrl: 'https://3npmcryhp6.eu-central-1.awsapprunner.com' // <-- AGGIORNATO CON L'URL DEL TUO BACKEND IN PRODUZIONE
 };
